f4d0732ac09c79e7a94badf5b532ec90b83c04eb
Mateus <mateusc2001@gmail.com>
2021-08-11 17:55:35 -0300

    Endpoint dos periféricos

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/perifericos/v1/tef/pinpad/verifica-presenca', (req, res) => {
+    res.status(401).send();
+});
+
 app.listen(PORT, () => console.log('Rodando na porta 3000'));
